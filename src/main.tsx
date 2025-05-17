330a6570ae94224557cc872225fed0c39bfe3b1a
Animish2002 <animishchopade123@gmail.com>
2025-05-17 13:40:50 +0530

    right click disabled for the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// 1. Disable text selection via CSS
+const style = document.createElement("style");
+style.textContent = `
+  * {
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+  }
+`;
+document.head.appendChild(style);
+
+// 2. Disable right-click via JavaScript
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+  return false;
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
