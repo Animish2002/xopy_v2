511aede9760d5e63b0aa402a7ecf0093ea1a884a
Animish2002 <animishchopade123@gmail.com>
2025-06-05 22:43:49 +0530

    url  change

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -20,7 +20,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     // Only create socket if we have a valid backend URL
-    const SOCKET_URL = "https://server.xopy.in/socket";
+    const SOCKET_URL = "https://server.xopy.in/";
 
     if (!SOCKET_URL || !user || user.role !== "SHOP_OWNER") return; // Don't connect if no user or URL or user is not shopowner
 
