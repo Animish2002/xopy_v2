9a96c29f40265a7657265bb49f16fb547382a02c
Vrushabh S Kulkarni <vrushabhskulkarni@gmail.com>
2025-07-10 00:45:43 +0530

    Color CHanges

--- src/EndUser/Preferences.jsx
+++ src/EndUser/Preferences.jsx
@@ -590,7 +590,7 @@ const Preferences = () => {
                 (!preferences.paperType || !preferences.noofCopies)) ||
               stage === stages.length - 1
             }
-            className={`flex items-center justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ${
+            className={`flex items-center justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ${
               (stage === 0 && files.length === 0) ||
               (stage === 2 &&
                 (!preferences.paperType || !preferences.noofCopies)) ||
